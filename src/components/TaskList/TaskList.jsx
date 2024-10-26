fbb84a1895174c24b7795d735f4f3b4d933ec4b6
ANJUM FERDOUS <anjumferdous110@gmail.com>
2024-10-26 12:28:13 +0600

    amployer dashboard

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div
+      id="tasklist"
+      className="flex overflow-x-auto  items-center justify-start gap-5 flex-nowrap h-[55%] w-full py-5 mt-10"
+    >
+      <div className="flex-shrink-0 h-full w-[300px] bg-red-400 p-5 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-3 py-1 rounded text-sm">High</h3>
+          <h4 className="text-sm">20 feb 2024</h4>
+        </div>
+
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          explicabo omnis ullam aliquid nisi.
+        </p>
+      </div>
+
+      <div className="flex-shrink-0 h-full w-[300px] bg-green-400 p-5 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-3 py-1 rounded text-sm">High</h3>
+          <h4 className="text-sm">20 feb 2024</h4>
+        </div>
+
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          explicabo omnis ullam aliquid nisi.
+        </p>
+      </div>
+
+      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 p-5 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-3 py-1 rounded text-sm">High</h3>
+          <h4 className="text-sm">20 feb 2024</h4>
+        </div>
+
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          explicabo omnis ullam aliquid nisi.
+        </p>
+      </div>
+
+      <div className="flex-shrink-0 h-full w-[300px] bg-gray-400 p-5 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-3 py-1 rounded text-sm">High</h3>
+          <h4 className="text-sm">20 feb 2024</h4>
+        </div>
+
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          explicabo omnis ullam aliquid nisi.
+        </p>
+      </div>
+
+      <div className="flex-shrink-0 h-full w-[300px] bg-red-400 p-5 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-3 py-1 rounded text-sm">High</h3>
+          <h4 className="text-sm">20 feb 2024</h4>
+        </div>
+
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          explicabo omnis ullam aliquid nisi.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default TaskList
